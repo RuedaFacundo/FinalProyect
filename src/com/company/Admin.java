68c2becbcf8e7ba95844b78698bc2f3b8d674c72
= <=>
2021-06-08 19:42:27 -0300

    Inclusion de Gson a la clase Admin

--- src/com/company/Admin.java
+++ src/com/company/Admin.java
@@ -1,6 +1,10 @@
 package com.company;
 
-public class Admin extends User {
+import com.google.gson.Gson;
+
+import java.io.*;
+
+public class Admin extends User implements Serializable {
 
 
     public Admin (){
@@ -11,9 +15,29 @@ public class Admin extends User {
         super(name, key, dni, phone, email);
     }
 
-    /// metodo agregar recepcionista
-    /// metodo agregar reserva
-    /// metodo back up de informacion
-    /// metodo verificar reserva
-    /// listar habitaciones ocupadas
+    File file = new File("admin.json");
+
+    public void writeFile (Admin admin)  {
+        Gson gson = new Gson();
+        try{
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            gson.toJson(admin, Admin.class, bufferedWriter);
+        } catch (IOException e){
+            System.out.println("El archivo no se pudo escribir" + e.getMessage());
+        }
+    }
+
+    public void readFile () throws FileNotFoundException {
+        Gson gson = new Gson();
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            Admin admin = gson.fromJson(bufferedReader, Admin.class);
+            System.out.println(admin);
+        } catch (FileNotFoundException e){
+            System.out.println("No se pudo abrir el archivo" + e.getMessage());
+        }
+    }
+
+
+
 }
